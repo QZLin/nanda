fe2f571ca9843a7cba221669a0934c0f6d11e38c
QZLin <qzlin01@163.com>
2020-07-12 15:26:15 +0800

    adjust mainloop func add collision detect

--- app_zero.py
+++ app_zero.py
@@ -10,47 +10,66 @@ class App:
 
     def __init__(self, width=800, height=600, tick=30):
         init()
+        self.width = width
+        self.height = height
         self.size = width, height
         self.screen: Surface = display.set_mode(self.size)
 
         self.tick = tick
         self.clock = time.Clock()
 
+        self.keep_going = True
         ###
+        self.bg = image.load('img/bg1.png')
 
         self.ball = image.load('img/zero.png')
         self.ball_rect: RectType = self.ball.get_rect()
+        self.ball_rect.x, self.ball_rect.y = 1, 1
 
-        self.velocity = 3, 3
+        self.velocity = [3, 3]
+
+    def event_handler(self, events):
+        for e in events:
+            if e.type == KEYDOWN:
+                if e.key in self.keys_arrow.keys():
+                    self.velocity = [x * 3 for x in self.keys_arrow[e.key]]
+
+            elif e.type == QUIT:
+                self.exit()
+
+    def update(self):
+        # self.ball_rect: RectType
+        x, y = self.ball_rect.x, self.ball_rect.y
+        print(x, y)
+        if x < 0 and self.velocity[0] < 0:
+            self.velocity[0] = 0
+        elif x > self.width and self.velocity[0] > 0:
+            self.velocity[0] = 0
+
+        if y < 0 and self.velocity[1] < 0:
+            self.velocity[1] = 0
+        elif y > self.height and self.velocity[1] > 0:
+            self.velocity[1] = 0
+
+        self.ball_rect = self.ball_rect.move(self.velocity)
 
     def draw(self):
-        self.screen.fill((0, 0, 0))
+        # self.screen.fill((0, 0, 0))
+        self.screen.blit(self.bg, (0, 0))
         self.screen.blit(self.ball, self.ball_rect)
         display.flip()
 
-    def update(self):
-        self.ball_rect = self.ball_rect.move(self.velocity)
-
-    @staticmethod
-    def exit():
-        print('Bye!')
-        quit()
-        exit()
-
     def mainloop(self):
-        while True:
-            self.clock.tick(self.tick)
-            for e in event.get():
-                e: EventType
-                if e.type == KEYDOWN:
-                    if e.key in self.keys_arrow.keys():
-                        self.velocity = self.keys_arrow[e.key]
-
-                elif e.type == QUIT:
-                    self.exit()
-
+        while self.keep_going:
             self.update()
             self.draw()
+            self.clock.tick(self.tick)
+            self.event_handler(event.get())
+
+    def exit(self):
+        print('Bye!')
+        self.keep_going = False
+        quit()
 
 
 app = App(tick=60)
