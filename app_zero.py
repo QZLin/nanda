22d7a1d15fb86fef65e844acb5f0e5afc91b15fc
QZLin <qzlin01@163.com>
2020-07-13 16:28:15 +0800

    character img manage -> sprite

--- app_zero.py
+++ app_zero.py
@@ -2,11 +2,12 @@ from math import sqrt
 from random import choice
 
 from pygame import KEYDOWN, K_UP, K_DOWN, K_LEFT, K_RIGHT, QUIT
-from pygame import Surface
 from pygame import init, image, display, quit, event, time
-from pygame.draw import circle
-from pygame.rect import RectType
+from pygame.sprite import Sprite, Group
 from pygame.transform import scale
+# Type Mark
+from pygame.rect import Rect
+from pygame import Surface
 
 
 def distance(x_y1, x_y2):
@@ -40,8 +41,47 @@ class Gird:
         for y in self.yy:
             for x in self.xx:
                 self.pts.append((x, y))
-        # print(pts)
         return self.pts
+
+
+class Char(Sprite):
+    ZERO = 0
+    MID = 1
+
+    def __init__(self, img, border_size, *groups, vx=0, vy=0):
+        self.image: Surface = img
+        self.screen_x, self.screen_y = border_size
+
+        self.rect: Rect = self.image.get_rect()
+        self.pixel_xy = self.image.get_width(), self.image.get_height()
+        # self.pos = [self.rect.x, self.rect.y]
+        self.vx, self.vy = vx, vy
+
+        super().__init__(*groups)
+
+    def set_pos(self, x, y, pos_type=ZERO):
+        if pos_type == self.ZERO:
+            # self.pos = [x, y]
+            self.rect.x, self.rect.y = x, y
+        elif pos_type == self.MID:
+            self.rect.x = x - self.pixel_xy[0]
+            self.rect.y = y - self.pixel_xy[1]
+
+    def update(self):
+        x, y = self.rect.x, self.rect.y
+        vx, vy = self.vx, self.vy
+        # print(x, y)
+        if x < 0 and vx < 0:
+            self.vx = 0
+        elif x > self.screen_x - 72 and vx > 0:
+            self.vx = 0
+
+        if y < 0 and vy < 0:
+            self.vy = 0
+        elif y > self.screen_y - 108 and vy > 0:
+            self.vy = 0
+
+        self.rect = self.rect.move(self.vx, self.vy)
 
 
 class Game:
@@ -49,8 +89,7 @@ class Game:
 
     def __init__(self, width=800, height=600, tick=30):
         init()
-        self.width = width
-        self.height = height
+        self.width, self.height = width, height
         self.size = width, height
         self.screen: Surface = display.set_mode(self.size)
 
@@ -59,12 +98,16 @@ class Game:
 
         self.keep_going = True
         ###
-        self.bg = image.load('img/bg1.png')
+        self.bg: Surface = image.load('img/bg1.png')
         self.crystal = scale(image.load('img/crystal.png'), (50, 50))
 
         self.chr = image.load('img/zero.png')
+        self.char = Char(scale(self.chr, (72, 108)), self.size)
+        self.sprites = Group()
+        self.sprites.add(self.char)
+
         self.chr = scale(self.chr, (72, 108))
-        self.chr_xy: RectType = self.chr.get_rect()
+        self.chr_xy: Rect = self.chr.get_rect()
         self.chr_xy.x, self.chr_xy.y = 1, 1
 
         self.velocity = [3, 3]
@@ -79,11 +122,13 @@ class Game:
             if e.type == KEYDOWN:
                 if e.key in self.keys_arrow.keys():
                     self.velocity = [x * 3 for x in self.keys_arrow[e.key]]
+                    self.char.vx, self.char.vy = [x * 3 for x in self.keys_arrow[e.key]]
 
             elif e.type == QUIT:
                 self.exit()
 
     def update(self):
+        self.sprites.update()
         self.__move()
         self.__collect()
 
@@ -109,7 +154,8 @@ class Game:
 
     def __collect(self):
         r = 50
-        px, py = self.chr_xy.x, self.chr_xy.y
+        # px, py = self.chr_xy.x, self.chr_xy.y
+        px, py = self.char.rect.x, self.char.rect.y
         for pt in self.dots:
             x, y = pt
             if abs(px - x) < r and abs(py - y) < r and \
@@ -124,7 +170,8 @@ class Game:
             self.screen.blit(self.crystal, pt)
             # circle(self.screen, (255, 0, 0), pt, 10)
 
-        self.screen.blit(self.chr, self.chr_xy)
+        self.sprites.draw(self.screen)
+        # self.screen.blit(self.chr, self.chr_xy)
         display.flip()
 
     def mainloop(self):
